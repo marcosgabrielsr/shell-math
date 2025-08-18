1d43d090ae1a058397d06833b02cddeb49a14d38
marcosdevse <marcosdevse@gmail.com>
2025-08-17 22:02:01 -0400

    feat: New version with C++

--- mathExp.h
+++
@@ -1,8 +0,0 @@
-// Including libraries
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Prototype functions
-// Removes the spaces from an math expression
-void removeSpaces(char exp[]);
