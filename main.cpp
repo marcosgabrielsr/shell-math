34c9596e4188ec407f44afd5742803e8dbedda5f
marcosdevse <marcosdevse@gmail.com>
2025-09-07 11:45:09 -0400

    feat: add functions(from main.cpp to token.h)  to print the tokens on terminal

--- main.cpp
+++ main.cpp
@@ -3,16 +3,7 @@
 
 using namespace std;
 
-// Functions prototype
-void allocVector(string* vet);
-
 int main() {
-    string* vet;
-    allocVector(vet);
-    
+   
     return EXIT_SUCCESS;
-}
-
-void allocVector(string* vet) {
-    vet = new string[17];
 }
--- token.h
+++ token.h
@@ -6,25 +6,26 @@
 using namespace std;
 
 enum Names {
-    UNDEF, //0
-    NUMBER, //1
-    DOUBLE_LITERAL, //2
-    FLOAT_LITERAL, //3
-    INTEGER_LITERAL, //4
-    OPSIGNAL, //5
-    SM, //6
-    SB, //7
-    MU, //8
-    DI, //9
-    GRSYMBOLS, //10
-    PAR_RIGHT, //11
-    PAR_LEFT, //12
-    BRACK_RIGHT, //13
-    BRACK_LEFT, //14
-    END_OF_FILE //15
+    UNDEF,            //0
+    NUMBER,           //1
+    DOUBLE_LITERAL,   //2
+    FLOAT_LITERAL,    //3
+    INTEGER_LITERAL,  //4
+    PLUS,             //5
+    MINUS,            //6
+    MULT,             //7
+    DIV,              //8
+    L_PARENTHESE,     //9
+    R_PARENTHESE,     //10
+    L_BRACKET,        //11
+    R_BRACKET,        //12
+    END_OF_FILE       //13
 };
 
 class Token {
+    private:
+        static string* vet;
+
     public:
         int name;
         int attribute;
@@ -38,5 +39,41 @@ class Token {
         Token(int name, int attr) {
             this->name = name;
             this->attribute = attr;
+        }
+
+        // Static methods to print the tokens names
+        static void allocVet() {
+            vet = new string[14];
+
+            vet[0] = "UNDEF";           //0
+            vet[1] = "NUMBER";          //1
+            vet[2] = "DOUBLE_LITERAL";  //2
+            vet[3] = "FLOAT_LITERAL";   //3
+            vet[4] = "INTEGER_LITERAL"; //4
+            vet[5] = "PLUS";            //5
+            vet[6] = "MINUS";           //6
+            vet[7] = "MULT";            //7
+            vet[8] = "DIV";             //8
+            vet[9] = "L_PARENTHESE";    //9
+            vet[10]= "R_PARENTHESE";    //10
+            vet[11]= "L_BRACKET";       //11
+            vet[12]= "R_BRACKET";       //12
+            vet[13] = "END_OF_FILE";    //13
+        }
+
+        static void freetVet() {
+            delete[] vet;
+        }
+
+        static void printTk(Token* t) {
+            cout << vet[t->name];
+
+            if(t->name == NUMBER) {
+                cout << "(" << t->lexeme << ")";
+            } else {
+                cout << "(" << t->attribute << ")";
+            }
+            
+            cout << " ";
         }
 };
